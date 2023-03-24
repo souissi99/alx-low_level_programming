400b0fec444bef248796827a6e451c7786f90c68
Souissi Omar <souissi.univers@gmail.com>
2023-03-24 16:46:15 -0700

    project 0x04

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * print_numbers - checks for checks for a digit (0 through 9).
- *
- * Return: Always 0.
- */
+* print_numbers - checks for checks for a digit (0 through 9).
+*
+* Return: Always 0.
+*/
 void print_numbers(void)
 {
  int c;
